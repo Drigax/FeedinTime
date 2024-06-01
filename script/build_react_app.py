aa7337a5e8c8101f319d9d66ae22c2ecad7e72bb
Nicholas Barlow <whoisdrigax@gmail.com>
2024-05-31 20:52:38 -0700

    update python build helper script to use custom site path, add gitmodule to react website to be hosted by this application

--- script/build_react_app.py
+++ script/build_react_app.py
@@ -1,15 +1,17 @@
 Import("env")
 import os
 
+site_root_path = "FeedinTime-Site"
+
 def before_build_spiffs(source, target, env):
     print("Building React App...")
-    env.Execute("cd web && npm run build")
+    env.Execute(f'cd {site_root_path} && npm run build')
     print("React App built!")
 
     print("Removing old SPIFFS image...")
     env.Execute("rm -rf data")
 
     print("Copying React App to SPIFFS...")
-    env.Execute("cp -r web/build data")    
+    env.Execute(f'cp -r {site_root_path}/build data')    
 
 env.AddPreAction("$BUILD_DIR/spiffs.bin", before_build_spiffs)
