3c15484302f4bce7ff30be14f0a2a532cf37dc69
Nicholas Barlow <whoisdrigax@gmail.com>
2024-05-31 20:59:04 -0700

    revert change to react app copy script

--- script/build_react_app.py
+++ script/build_react_app.py
@@ -2,7 +2,6 @@ Import("env")
 import os
 
 site_root_path = "FeedinTime-Site"
-on_device_site_root_path = "web"
 
 def before_build_spiffs(source, target, env):
     print("Building React App...")
@@ -13,6 +12,6 @@ def before_build_spiffs(source, target, env):
     env.Execute("rm -rf data")
 
     print("Copying React App to SPIFFS...")
-    env.Execute(f'cp -r {on_device_site_root_path}/build data')    
+    env.Execute(f'cp -r {site_root_path}/build data')    
 
 env.AddPreAction("$BUILD_DIR/spiffs.bin", before_build_spiffs)
