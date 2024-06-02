91426ebe82417c091e2fbd9cb3ad7f9d0f009f69
Nicholas Barlow <whoisdrigax@gmail.com>
2024-06-02 13:59:12 -0700

    Update partition table to 2MB SPIFFS partition, 2MB app partiion, update platform.io configuration to use parition table, update main.c, adding server initialization, wifi client connection, and basic hosting callbacks. Update site dependency, add spiffs parition generation to build for FeedinTime, update build helper script to also shorten file path depth by moving conents of /static to /

--- script/build_react_app.py
+++ script/build_react_app.py
@@ -12,6 +12,10 @@ def before_build_spiffs(source, target, env):
     env.Execute("rm -rf data")
 
     print("Copying React App to SPIFFS...")
-    env.Execute(f'cp -r {site_root_path}/build data')    
+    env.Execute(f'cp -r {site_root_path}/build data')
+
+    print ("Moving site /static assets to /data root to reduce filepath...")
+    env.Execute(f'rm -rf data/*/*.map data/*/*.LICENSE.txt')
+    env.Execute(f'mv data/static/* data/ && rmdir data/static')
 
 env.AddPreAction("$BUILD_DIR/spiffs.bin", before_build_spiffs)
--- src/main.c
+++ src/main.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
+#include <string.h>
 #include <inttypes.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <WiFi.h>
+
+// ESP32 Specific includes, consider refactoring to header with platform specific defines.
 #include "esp_chip_info.h"
 #include "esp_flash.h"
 #include "esp_system.h"
+#include <ESPAsyncWebServer.h>
+#include "SPIFFS.h"
 
-void app_main() {
-    printf("Hello world!\n");
+//TODO: move versioning info to a config file.
+const int versionMajor = 0;
+const int versionMinor = 1;
+const int versionHotfix = 0;
 
+//TODO: move ssid and password to a config file.
+const char* ssid = "SSID"; //replace with actual ssid and password
+const char* password = "PASSWORD";
+int webserverPort = 80;
+
+void restart(){
+    for (int i = 10; i >= 0; i--) {
+        printf("Restarting in %d seconds...\n", i);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
+}
+
+void printSystemInfo(){
     /* Print chip information */
     esp_chip_info_t chip_info;
     uint32_t flash_size;
@@ -34,12 +58,75 @@ void app_main() {
            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
     printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
 }
+
+void notFound(AsyncWebServerRequest *request)
+{
+    if (request->method() == HTTP_OPTIONS)
+    {
+        request->send(200);
+    }
+    else
+    {
+        request->send(404, "application/json", "{\"message\":\"Not found\"}");
+    }
+}
+
+void app_main() {
+    printf("Initializing FeedinTime v{}");
+    printSystemInfo();
+
+    // Connect to wifi network as client.
+    WiFi.mode(WIFI_STA); //Optional
+    WiFi.begin(ssid, password);
+    printf("\nConnecting to %s\n", ssid);
+
+    while(WiFi.status() != WL_CONNECTED){
+        Serial.print(".");
+        delay(100);
+    }
+    printf("\nConnected to %s.\n", ssid);
+    printf("Local ESP32 IP: ");
+    printf(WiFi.localIP());
+    printf("\n");
+
+    // Initialize Web server.
+    printf("Initializing web server on port %d.\n", webserverPort);
+    AsyncWebServer server(webserverPort);
+
+    // Initialize file system
+    printf("\nInitializing SPIFFS file system.\n");
+    if (!SPIFFS.begin(true))
+    {
+        printf("An Error has occurred while mounting SPIFFS. Restarting...\n");
+        restart();
+        return;
+    }
+
+    // Allow Any URL to make calls to endpoints.
+    printf("\nInitializing webserver default headers.\n");
+    DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
+    DefaultHeaders::Instance().addHeader("Access-Control-Allow-Methods", "GET, POST, PUT");
+    DefaultHeaders::Instance().addHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    // Serve the filesystem
+    printf("Serving contents of FeedinTime/data/ at / and /static/ .\n");
+    server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
+    server.serveStatic("/static/", SPIFFS, "/");
+
+    // /schedule -> send json document of feeding schedule.
+    printf("Setting up callback for /schedule .\n");
+    server.on("/schedule", HTTP_GET, [](AsyncWebServerRequest *request)
+        {
+        String out;
+        serializeJson(feedingSchedule, out);
+        request->send(200, "text/json", out);
+    });
+
+    // in case we have a request that we cannot serve.
+    server.onNotFound(notFound);
+
+    // Finally, start the webserver.
+    server.begin();
+}
+
